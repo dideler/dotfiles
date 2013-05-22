7e3d856b896d1bee9c1316716fc4ad275bdc9428
Dennis Ideler <ideler.dennis@gmail.com>
2013-05-22 11:48:02 -0400

    Convert aliases to fish. Also improve some and remove some

--- config.fish
+++ config.fish
@@ -11,6 +11,8 @@
 # - more on lists: http://fishshell.com/tutorial.html#tut_lists
 #
 # Tips:
+# - use alt+up (instead of !$) to get last part of previous command
+#   - note: doesn't work if you hit up first
 # - use alt+left and alt+right to skip words in the prompt
 # - use alt+left and alt+right to navigate directory history if prompt is empty
 # - use math for calculations
@@ -25,137 +27,193 @@ set fish_greeting # Clear greeting
 # If an alias requires special usage (e.g. an argument) it will say so in the
 # comments. Otherwise assume that the alias requires no arguments.
 
-alias git hub # Wrapper for git
+alias alert 'notify-send --urgency=low --icon=(test $status = 0; and echo terminal; or echo error)'
+# An "alert" for long running commands. Icon is determined by exit status.
+# E.g. sleep 10; alert title message
 
+alias play-music 'exec mplayer -shuffle -really-quiet ~/Music/**'
+# Plays all my songs in random order then exits.
 
-alias ..='cd ..'
-alias ...='cd ../..'
-alias aliasloc='/home/dennis/.bash_aliases'
-alias addgpl='wget -q http://www.gnu.org/licenses/gpl.txt -O LICENSE'
-alias addcrapl='wget -q http://matt.might.net/articles/crapl/CRAPL-LICENSE.txt -O LICENSE'
-alias bbc='ssh 192.168.1.2'
-alias clr='echo "BAD DENNIS! Use ctrl+L"'
-alias cls='clear; ls'
-alias distro='cat /etc/*-release' # 'lsb_release -rd' also works
-alias grep='grep --colour'
-alias rgrep='rgrep --colour'
-alias ipconfig='ifconfig'
-alias l='ls -rt'
-alias la='ls -A'
-alias ll='ls -l'
-alias ls='ls --color=auto'
-alias lsg='ls | grep'
-alias lsp='ls -1 /var/log/packages/ > package-list'
-alias na='nano -c'
-alias nano='nano -c'
-alias sandcastle='ssh di07ty@sandcastle.cosc.brocku.ca'
-alias sano='sudo nano'
-alias tarbz2='tar -jxvf'
-alias targz='tar -zxvf'
-alias temp='sensors 2>/dev/null'
-alias thoughtstack='java -jar ThoughtStack.jar'
-alias ins='sudo apt-get install'
-alias install='sudo apt-get install'
-alias upd='sudo apt-get update'
-alias update='sudo apt-get update'
-alias upg='sudo apt-get upgrade'
-alias upgrade='sudo apt-get upgrade'
-alias rem='sudo apt-get remove' # Just removes the package.
-alias remove='sudo apt-get remove' # Just removes the package.
-alias purge='sudo apt-get purge' # Completely removes a package and its configuration files.
-alias rep='sudo add-apt-repository' # Add a new PPA to the repository.
-alias repository='sudo add-apt-repository'
-alias web='links -g -download-dir ~/ www.google.com'
-alias execbin='chmod +x'
-alias bookmarks='nano /home/dennis/.gtk-bookmarks'
-alias xmleditor='cd /opt/bin/; ./serna-free-4.2'
-alias mp='mplayer -quiet -playlist' # For playing a playlist, such as .pls.
-alias m='mplayer -quiet'
-alias md5='md5sum'
-alias cman='man 3'
-alias lsd='ls -d */' # List only directories in current directory.
-alias disk='df -hT' # Disk space + file system type.
-alias ram='free -m' # RAM usage in MB.
-alias javadecompiler='/home/dennis/Downloads/jd-gui'
-alias t='python ~/.t/t.py --task-dir ~/tasks --list tasks'
-alias updatesite='scp index.html di07ty@sandcastle.cosc.brocku.ca:public_html/'
-alias pngcrush='pngcrush -rem alla -reduce -brute'
-alias mac='ifconfig | grep HWaddr'
-alias convertvideo='mencoder -oac mp3lame -ovc lavc -o' # example: convertvideo video.ogg output.avi
-alias minecraft='java -Xmx1024M -Xms512M -cp Downloads/minecraft.jar net.minecraft.LauncherFrame'
-alias dirsize='du -ch | grep total | cut -f 1'  # Total size of the current directory (human readable)
-alias size='du -h' # Human readable size of the file/directory you pass it.
-alias units='units --verbose'
-alias pinknoise='play -t sl -r48000 -c2 - synth -1 pinknoise tremolo .1 40 <  /dev/zero'
-alias whitenoise='cat /dev/urandom | aplay -f cd'
-alias snipe='ps -x | grep' # Snipe an unresponsive process to get its pid to kill it.
-alias external_ip='curl ifconfig.me'
+alias mp 'mplayer -really-quiet -playlist' # For playing a playlist.
 
-alias eth0ip="/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \"eth: \" \$1}'"
+alias m 'mplayer -really-quiet'
+
+alias ... 'cd ../..'
+
+alias addgpl 'wget -q http://www.gnu.org/licenses/gpl.txt -O LICENSE'
+
+alias addcrapl 'wget -q http://matt.might.net/articles/crapl/CRAPL-LICENSE.txt -O LICENSE'
+
+alias clr 'echo "BAD DENNIS! Use ctrl+L"'
+
+alias cls 'clear; and ls'
+
+alias distro 'cat /etc/os-release | head --lines=2'
+# 'lsb_release -rd' also works but doesn't contain the distro's codename.
+
+#alias grep grep --colour' # TODO
+
+#alias rgrep rgrep --colour' # TODO
+
+alias ipconfig ifconfig
+
+#alias l ls -rt' # TODO
+
+#alias la ls -A'
+
+#alias ll ls -l'
+
+#alias ls ls --color=auto'
+
+#alias lsg ls | grep'
+
+#alias lsp ls -1 /var/log/packages/ > package-list'
+
+alias lsd 'ls -d */' # List all directories in current directory.
+
+alias nano 'nano --const' # Constantly show the cursor position in nano.
+
+alias sandcastle 'ssh di07ty@sandcastle.cosc.brocku.ca'
+
+#alias tarbz2 tar -jxvf' # TODO: make a function to handle different tars
+
+#alias targz tar -zxvf'
+
+alias temp 'sensors 2>/dev/null'
+
+alias ins 'sudo apt-get install'
+
+alias install 'sudo apt-get install'
+
+alias upd 'sudo apt-get update'
+
+alias update 'sudo apt-get update'
+
+alias upg 'sudo apt-get upgrade'
+
+alias upgrade 'sudo apt-get upgrade'
+
+alias rem 'sudo apt-get remove' # Removes the package.
+
+alias remove 'sudo apt-get remove' # Removes the package.
+
+alias purge 'sudo apt-get purge' # Removes the package and its configuration files.
+
+alias rep 'sudo add-apt-repository' # Add a new PPA to the repository.
+
+alias repository 'sudo add-apt-repository'
+
+alias web 'links2 -g -download-dir ~/Downloads/ www.google.com'
+# Browse the web in a basic browser.
+
+alias bookmarks 'eval $EDITOR /home/dennis/.gtk-bookmarks'
+# Opens the file with your bookmarked directories.
+
+alias md5 md5sum
+
+alias cman 'man 3' # Manual pages from the library section.
+
+alias disk 'df -hT' # Disk space + file system type.
+
+alias show-ram 'free --human' # RAM usage in human-readable form.
+
+alias java-decompiler '/usr/local/bin/jd-gui'
+
+alias t 'python ~/.t/t.py --task-dir ~/tasks --list tasks'
+
+alias update-di07ty 'scp index.html di07ty@sandcastle.cosc.brocku.ca:public_html/'
+
+alias pngcrush 'pngcrush -rem alla -reduce -brute'
+# pngcrush is best used when the options are optimized for the image.
+# For something easier, try an online tool like http://kraken.io.
+
+alias mac 'ifconfig | grep HWaddr | awk \'{print $1 " " $5}\''
+# Shows the MAC address of your network devices.
+
+alias convertvideo 'mencoder -oac mp3lame -ovc lavc -o'
+# example: convertvideo video.ogg output.avi
+
+alias minecraft 'java -Xmx1024M -Xms512M -cp ~/games/minecraft.jar net.minecraft.LauncherFrame'
+
+alias dirsize 'du -ch | grep total | cut -f 1'
+# Total size of the current directory (human readable)
+
+alias size 'du --human-readable'
+# Human readable size of the file/directory you pass it.
+
+alias units 'units --verbose'
+# Converts units (e.g. kg to lbs).
+
+alias pinknoise 'play -t sl -r48000 -c2 - synth -1 pinknoise tremolo .1 40 <  /dev/zero'
+
+alias whitenoise 'cat /dev/urandom | aplay -f cd'
+
+alias snipe 'ps -x | grep --ignore-case'
+# Handy for finding a process (e.g. when it's unresponsive).
+
+alias external_ip 'curl ifconfig.me'
+
+alias eth0ip "/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \"eth: \" \$1}'"
 # Shows your wired IP address.
 
-alias wlan0ip="/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \"wlan: \" \$1}'"
+alias wlan0ip "/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \"wlan: \" \$1}'"
 # Shows your wireless IP address.
 
-alias serve='eth0ip; wlan0ip; python -m SimpleHTTPServer'
+alias serve 'eth0ip; and wlan0ip; and python -m SimpleHTTPServer'
 # Serves current directory on local network, port 8000.
 
-alias broadcast='echo "Client can watch your terminal live with '\''nc your_ip 5000'\''.";
-                 script -qf | tee >(nc -l -p 5000)'
+alias broadcast 'echo "Client can watch your terminal live with \'nc your_ip 5000\'."; and script -qf | tee >(nc -l -p 5000)'
 # Streams your terminal for others to view.
-# Warning: NOT YET TESTED
+# Warning: NOT YET TESTED. Do not use for pair programming!
 # Note: Supposedly to broadcast over UDP on port 5000, use:
 #       script -qf >(nc -ub 192.168.1.255 5000)
 
-alias pushhh='pushd $@ > /dev/null'
-# Silence pushd. Error messages appear on stderr.
-# Usage: pushd dir1, dir2, ...
-
-alias imagify='jp2a --colors'
+alias imagify 'jp2a --colors'
 # Displays a JPEG image as coloured ASCII in stdout.
+# Also check out picture-tube which is for PNGs.
 # Usage: imagify file1, file2, ...
 
-alias vi='vi -O'
+alias vi 'vi -O'
 # Vertical-split windows if multiple files given.
 # Usage: vi file1, file2, ...
 
-alias combineimgs='convert +append'
+alias combineimgs 'convert +append'
 # Combines images horizontally (side-by-side).
 # Usage: combineimgs infile1, infile2, ..., outfile
 
-alias combinepdfs='convert -density 200'
+alias combinepdfs 'convert -density 200'
 # Combines PDFs vertically.
 # Usage: combinepdfs infile1, infile2, ..., outfile
 
-alias spaces2tabs="sed -i 's/ \+ /\t/g'"
+alias spaces2tabs "sed -i 's/ \+ /\t/g'"
 # Replaces multiple spaces with a tab.
 # Usage: spaces2tabs textfile
 
-alias chromecache='cd ~/.cache/google-chrome/Default/Cache/ && file $(ls -rt | tail)'
+alias chromecache 'cd ~/.cache/google-chrome/Default/Cache/; and file (ls -rt | tail)'
 # Brings you to Chrome's cache directory and shows filetypes for recent files.
 
-alias showpkg='apt-cache showpkg'
+alias showpkg 'apt-cache showpkg'
 # Shows some general information for a single package.
 # Usage: showpkg package_name
 
-alias search='apt-cache search'
+alias search 'apt-cache search'
 # Searches the package list for a regex pattern.
 # Usage: search some_pattern
 
-alias copysshkey="xclip -sel clip < ~/.ssh/id_rsa.pub && echo 'copied to cliboard'"
+alias copysshkey "xclip -sel clip < ~/.ssh/id_rsa.pub; and echo 'copied to cliboard'"
 # Copies your ssh key to your clipboard.
 
-alias remove='rm -iv'
+alias remove 'rm -iv'
 # A safer rm. Prompts you and tells you what has been deleted.
 # Usage: remove file1, file2, ...
 
-alias kernelversion="uname -mrs"
+alias kernelversion "uname -mrs"
 # Shows the Linux kernel version (e.g. Linux 3.2.0-38-generic x86_64)
 
-alias restartshell='exec $SHELL'
+alias restartshell 'exec $SHELL'
 # Restarts the shell so new changes can take effect.
 
-alias difference='grep --fixed-strings --line-regexp --invert-match --file'
+alias difference 'grep --fixed-strings --line-regexp --invert-match --file'
 # Shows the difference between two text files, one item per line.
 # Usage: difference file1 file2
 # Notes:
@@ -163,30 +221,30 @@ alias difference='grep --fixed-strings --line-regexp --invert-match --file'
 #   Alternative: comm -13 <(sort file1) <(sort file2)
 #   Alternative: diff file1 file2 | grep '>'
 
-alias bitch,='sudo'
+alias bitch, sudo
 # Sometimes your computer doesn't obey unless you threaten it.
 
-alias copy='xclip -selection clip <'
+alias copy 'xclip -selection clip <'
 # Copies the contents of a text file to your clipboard.
 # Assumes you're running X.
 # Usage: copy file
 
 ### Git aliases (some from @holman)
-alias gl='git log --oneline --decorate' # Also shows tags!
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gca='git commit -a'
-alias gcam='git commit -am'
-alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' # Upgrade your git if -sb breaks for you.
-alias ga='git add'
-#alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias grm="gs | grep ' D ' | sed 's/^ D //' | xargs git rm" # Holman's version doesn't handle whitespaces.
-alias git='hub' # See http://defunkt.io/hub/
-alias ggrep='git grep --line-number --heading --break --show-function' # greps files tracked with git.
-alias gcb="gb | grep \* | sed 's/^* //'" # Show git's current branch.
-alias gup='git fetch && git rebase -p origin/$(gcb)' # A friendlier git pull --rebase.
+alias gl 'git log --oneline --decorate' # Also shows tags!
+alias glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp 'git push origin HEAD'
+alias gd 'git diff'
+alias gc 'git commit'
+alias gcm 'git commit -m'
+alias gca 'git commit -a'
+alias gcam 'git commit -am'
+alias gco 'git checkout'
+alias gb 'git branch'
+alias gs 'git status -sb' # Upgrade your git if -sb breaks for you.
+alias ga 'git add'
+#alias grm "git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias grm "gs | grep ' D ' | sed 's/^ D //' | xargs git rm" # Holman's version doesn't handle whitespaces.
+alias git hub # Git wrapper. See http://defunkt.io/hub/
+alias ggrep 'git grep --line-number --heading --break --show-function' # greps files tracked with git.
+alias gcb "gb | grep \* | sed 's/^* //'" # Show git's current branch.
+alias gup 'git fetch; and git rebase -p origin/(gcb)' # A friendlier git pull --rebase.

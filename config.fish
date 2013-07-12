74846e09621efe9bf3ff4c70a40e10a8fc0f2db8
Dennis Ideler <ideler.dennis@gmail.com>
2013-07-12 18:31:17 -0400

    Show ANSI colour sequences in less and auto-install hub

--- config.fish
+++ config.fish
@@ -42,6 +42,34 @@
 
 set fish_greeting # Clear greeting
 
+# Show ANSI colour sequences in less when viewing man pages.
+set -x LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+set -x LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+set -x LESS_TERMCAP_me \e'[0m'           # end mode
+set -x LESS_TERMCAP_se \e'[0m'           # end standout-mode
+set -x LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+set -x LESS_TERMCAP_ue \e'[0m'           # end underline
+set -x LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+# Install hub for github if we don't have it yet.
+begin # Create a block scope so variables created exist only in this scope.
+  if test (uname) = Linux
+    if test (which hub) != /home/(whoami)/bin/hub
+      read -l -p "echo 'Install hub? [Y/n] '" input
+      switch $input
+        case '' 'y' 'Y'
+          curl http://github.github.com/hub/standalone -sLo ~/bin/hub
+          chmod +x ~/bin/hub
+          hub version
+        case '*'
+          echo 'Aborting hub installation'
+      end
+    end
+  else
+    echo 'Follow the instructions at https://github.com/github/hub to install hub'
+  end
+end
+
 ##########
 # Aliases 
 ##########

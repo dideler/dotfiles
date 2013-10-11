f02372b116d4c903a08ff95ea96604c8b0e79df7
Dennis Ideler <ideler.dennis@gmail.com>
2013-10-10 20:53:52 -0400

    Improve my fake cafe experience
    - Add option to specify volume
    
    - Reduce default volume to 50% (70% was too loud)

--- functions/coffitivity.fish
+++ functions/coffitivity.fish
@@ -1,3 +1,8 @@
-function coffitivity --description "Loop sounds from a coffee shop and run in the background"
-  mplayer -really-quiet -loop 0 -volume 70 ~/Music/coffitivity.mp3 </dev/null ^/dev/null &
+function coffitivity --description "Loop sounds from a coffee shop and run in the background. Plays at half volume by default."
+  if test (count $argv) = 1
+    set volume $argv[1]
+  else
+    set volume 50
+  end
+  mplayer -really-quiet -loop 0 -volume $volume ~/Music/coffitivity.mp3 </dev/null ^/dev/null &
 end

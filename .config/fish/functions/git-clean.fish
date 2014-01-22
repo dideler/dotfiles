214d4dee62a9856b8295047858428c4f0b7c7515
Dennis Ideler <dennis@landlordwebsolutions.com>
2014-01-22 14:31:20 -0500

    Update git-clean
    - Remove directory option; I usually want to clean files
    
    - Use long options for readability

--- .config/fish/functions/git-clean.fish
+++ .config/fish/functions/git-clean.fish
@@ -1,9 +1,9 @@
-function git-clean --description 'Cleans your working copy of untracked files and directories'
-  git clean -dfn
+function git-clean --description 'Cleans your working copy of untracked files (not directories)'
+  git clean --dry-run --force
   read -l -p 'set_color green; echo "Continue? [Yn]"; set_color normal; echo "> "' input
   switch $input
     case '' 'y' 'Y' 'yes' 'Yes' 'YES'
-      git clean -df
+      git clean --force
     case '*'
       return
   end

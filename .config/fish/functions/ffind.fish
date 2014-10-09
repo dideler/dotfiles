ef02d6f3c5587c546e0f5a80f09db2d3202e5b0d
Dennis Ideler <ideler.dennis@gmail.com>
2014-10-09 15:00:59 +0100

    Improve description for ffind

--- .config/fish/functions/ffind.fish
+++ .config/fish/functions/ffind.fish
@@ -1,4 +1,4 @@
 # Note: If query contains spaces, make sure to escape them manually.
-function ffind -d "Searches recursively from cwd for files containing the query"
+function ffind -d "Searches recursively from cwd for files containing the case-insensitive query"
   find . -iname "*"$argv"*" ^/dev/null
 end

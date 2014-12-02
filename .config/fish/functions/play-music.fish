bf1cb56952b46382dc71383eebf5ee7f06ab0bc9
Dennis Ideler <ideler.dennis@gmail.com>
2014-12-02 12:53:00 +0000

    Improve play-music function
    - Use a simpler name: music
    - Play only mp3 files to prevent error messages for playlists, etc.

--- .config/fish/functions/play-music.fish
+++ .config/fish/functions/play-music.fish
@@ -1,3 +1,3 @@
-function play-music --description "Plays all music in ~/Music then terminates"
-  exec mplayer -msgcolor -shuffle -quiet ~/Music/** ^/dev/null
+function music --description "Plays all music in ~/Music then terminates"
+  exec mplayer -msgcolor -shuffle -quiet ~/Music/**.mp3 ^/dev/null
 end

a6d835c6d6a0bbac708b7908d8852adf616fff12
Dennis Ideler <ideler.dennis@gmail.com>
2014-12-02 12:57:02 +0000

    Add recent-music function
    Also rename play-music.fish to music.fish

---
+++ .config/fish/functions/recent-music.fish
@@ -0,0 +1,5 @@
+function recent-music --description "Plays all songs in ~/Music, sorted by recent music first"
+  pushd ~/Music
+  mplayer -quiet (ls -t **.mp3) | sed -n -e 's/Playing //p'
+  popd
+end

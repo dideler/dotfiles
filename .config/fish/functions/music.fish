83dfd9585fa1fb84d3d18ceba5211c29d53cf6cd
Dennis Ideler <ideler.dennis@gmail.com>
2014-12-11 17:51:46 +0000

    Improve music script
    - Don't auto-close tab when done playing music
    - Play m4a's in addition to mp3's
    - Only show song titles

--- .config/fish/functions/music.fish
+++ .config/fish/functions/music.fish
@@ -1,3 +1,5 @@
-function music --description "Plays all music in ~/Music then terminates"
-  exec mplayer -msgcolor -shuffle -quiet ~/Music/**.mp3 ^/dev/null
+function music --description "Plays all music in ~/Music"
+  pushd ~/Music
+  mplayer -msgcolor -shuffle -quiet **.{mp3,m4a} | sed -n -e 's/Playing //p'
+  popd # FIXME: Doesn't go back to the previous directory.
 end

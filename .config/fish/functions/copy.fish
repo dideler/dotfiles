c024f3371cbdb4d9819cc584e49c12b4bd2ed3c0
Dennis Ideler <ideler.dennis@gmail.com>
2015-03-04 18:00:06 +0000

    Make copy function work on OS X and Linux

--- .config/fish/functions/copy.fish
+++ .config/fish/functions/copy.fish
@@ -3,7 +3,12 @@
 function copy --description "Copy the contents of a text file to your clipboard"
   set --local argc (count $argv)
   if test $argc -eq 1
-    xclip -selection clip < $argv[1]
+    switch (uname)
+      case 'Linux'
+        xclip -selection clip < $argv[1]
+      case 'Darwin'
+        pbcopy < $argv[1]
+    end
   else
     echo "Well this is embarrassing... I can only copy one file at a time."
   end

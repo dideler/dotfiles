40ff453fcb65f1ee6571cd89c255f3dad8414209
Dennis Ideler <ideler.dennis@gmail.com>
2015-06-26 20:41:52 +0100

    Run setup from the proper directory
    Needed to find the Brewfiles.
    Alternatively, the path given to the Brewfiles could be updated...

--- .config/fish/functions/setup-tools.fish
+++ .config/fish/functions/setup-tools.fish
@@ -1,18 +1,17 @@
 function setup-tools --description "General setup script (compatible with Ubuntu or OS X)"
-  # TODO: Test that file location for brew bundle works.
+  pushd ~/github/dideler/dotfiles
   switch (uname)
     case 'Linux'
       setup-linux  # Sets up linuxbrew, rbenv, and aliases.
       brew tap homebrew/bundle
       brew bundle --file=Brewfile.linux
     case 'Darwin'
-      echo "os setup"
-      # setup-mac  # Sets up homebrew, rbenv, aliases, and OS X config.
-      # brew tap homebrew/bundle
-      # brew bundle --file=Brewfile.osx
+      setup-mac  # Sets up homebrew, rbenv, aliases, and OS X config.
+      brew tap homebrew/bundle
+      brew bundle --file=Brewfile.osx
     case '*'
       echo "OS not recognized, skipping OS-specific setup routine."
   end
-
+  popd
   brew bundle
 end

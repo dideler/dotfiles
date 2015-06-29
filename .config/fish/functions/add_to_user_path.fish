ddd22cb8cb552f4bd49d90dabd0150b7e9714dab
Dennis Ideler <ideler.dennis@gmail.com>
2015-06-29 17:30:25 +0200

    Also export fish_user_paths when adding path
    This isn't required for PATH to be exported (that happens by default).
    This is just so fish_user_paths is exported to child processes, in case
    you happen to be inspecting that env var and are wondering why stuff
    might be "missing".

--- .config/fish/functions/add_to_user_path.fish
+++ .config/fish/functions/add_to_user_path.fish
@@ -1,3 +1,3 @@
 function add_to_user_path --description "Persistently prepends to your PATH via fish_user_paths"
-  set --universal fish_user_paths $fish_user_paths $argv
+  set --universal --export fish_user_paths $fish_user_paths $argv
 end

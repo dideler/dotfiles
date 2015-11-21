b0701d993edac22e15792e5f94e4953461f339f2
Dennis Ideler <ideler.dennis@gmail.com>
2015-11-21 23:40:10 +0000

    Add function for downloading Chrome extensions
    Credit: https://gist.github.com/paulirish/78d6c1406c901be02c2d#option-1-command-line-download-extension-as-zip-and-extract

---
+++ .config/fish/functions/chrome-dl.fish
@@ -0,0 +1,8 @@
+function chrome-dl --description "Download Chrome extensions, given their IDs"
+  for extension_id in $argv
+    curl --output "$extension_id.zip" \
+         --location "https://clients2.google.com/service/update2/crx?response=redirect&os=mac&arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=stable&prodversion=44.0.2403.130&x=id%3D$extension_id%26uc"
+    unzip -d "$extension_id-source" "$extension_id.zip"
+    rm "$extension_id.zip"
+  end
+end

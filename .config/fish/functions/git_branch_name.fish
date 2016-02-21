f4ccdc01160de58784a800e472d313fbde07a67c
Dennis Ideler <ideler.dennis@gmail.com>
2016-02-21 03:20:45 +0000

    Do not check if in git repo
    - Fixes bug where you are in a git repo but not at the root directory
      (so .git would not be found)
    
    - There is no significant speed difference with or without the check.
      However, if you were to use the git_is_repo function instead, there
      would be a significant speed difference. From my benchmarks, checking
      if there is a git repo when you are in a git repo sub directory takes
      roughly twice as long than just checking and redirecting to /dev/null!

--- .config/fish/functions/git_branch_name.fish
+++ .config/fish/functions/git_branch_name.fish
@@ -1,3 +1,3 @@
 function git_branch_name -d "Prints name of current git branch"
-  test -d .git; and command git symbolic-ref --short HEAD ^/dev/null
+  command git symbolic-ref --short HEAD ^/dev/null
 end

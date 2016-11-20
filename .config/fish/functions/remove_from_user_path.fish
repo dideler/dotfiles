88444e632ead49ab0f383b25d8e39088b6f5508f
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-20 16:36:15 -0400

    Fix fish function remove_from_user_path
    This function was no longer working. My guess is that it either never
    worked before, or fish_user_paths used to be a single string instead
    of a list.
    
    - Treat fish_user_paths as a list, use indices to remove paths
    
    - Accept multiple paths to remove

--- .config/fish/functions/remove_from_user_path.fish
+++ .config/fish/functions/remove_from_user_path.fish
@@ -1,3 +1,7 @@
-function remove_from_user_path --description "Removes the given path from fish_user_paths"
-  set -Ux fish_user_paths (echo $fish_user_paths | sed 's| '$argv[1]'||g')
+function remove_from_user_path --description "Removes the given paths from fish_user_paths"
+  for path in $argv
+    if set --local index (contains --index $path $fish_user_paths)
+      set --erase --universal fish_user_paths[$index]
+    end
+  end
 end

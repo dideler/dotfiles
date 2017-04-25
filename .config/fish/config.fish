8f3c5d4b7c6df40f048abfa59dc0e360dddc83d7
Dennis Ideler <ideler.dennis@gmail.com>
2017-04-25 13:17:35 +0100

    Fix fish alias grm
    It uses the gs alias which was converted to an abbreviation, so the grm
    alias was failing.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -108,7 +108,7 @@ alias glog "git log --graph \
             --abbrev-commit \
             --date=relative \
             --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset'"
-alias grm "gs | grep ' D ' | sed 's/^ D //' | xargs git rm"
+alias grm "git status -sb | grep ' D ' | sed 's/^ D //' | xargs git rm"
 alias git hub
 alias ggrep 'git grep --line-number --heading --break --show-function'
 alias gup 'git fetch --all; and git rebase --preserve-merges origin/(git_branch_name)'

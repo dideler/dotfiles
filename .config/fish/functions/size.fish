f4794158614583f7d42da8e17271f32a3d8c124c
Dennis Ideler <ideler.dennis@gmail.com>
2017-10-15 18:06:38 +0100

    :bug: Use a more precise size function

--- .config/fish/functions/size.fish
+++ .config/fish/functions/size.fish
@@ -1,4 +1,11 @@
 function size --description "Human readable size of the given file/dir"
-  du -chd 1 $argv[1] | grep total | cut -f 1
+  ls -gh $argv[1] | awk '{ print $4 }'
+  # du -chd 1 $argv[1] | grep total | cut -f 1
+  #
   # BSD tools don't have long options --total --human-readable --max-depth=1
+  #
+  # du doesn't give the size of the file, it gives an indication of how much
+  # space the file uses, which is subtly different (usually the size reported by
+  # du is the size of the file rounded up to the nearest number of blocks, where
+  # a block is typically 512B or 1kB or 4kB)
 end

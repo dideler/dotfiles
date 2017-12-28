78f16c4ee3f57f5ac514bc68c15d4f15d16dbcc9
Dennis Ideler <ideler.dennis@gmail.com>
2017-12-28 17:41:02 +0000

    Support copying variables in addition to files
    Can now differentiate between variables and files. E.g.
    
        copy $SENSITIVE_VAR
        copy README.md
    
    Not strictly checking if argument is a variable and is echo-able, but
    good enough for my needs right now.

--- .config/fish/functions/copy.fish
+++ .config/fish/functions/copy.fish
@@ -1,13 +1,21 @@
 # Usage: copy <file>
 
-function copy --description "Copy the contents of a text file to your clipboard"
+function copy --description "Copy the contents of a text file or variable to your clipboard"
   set --local argc (count $argv)
   if test $argc -eq 1
     switch (uname)
       case 'Linux'
-        xclip -selection clip < $argv[1]
+        if test -e $argv[1]
+          xclip -selection clip < $argv[1]
+        else
+          echo $argv[1] | xclip -selection clip
+        end
       case 'Darwin'
-        pbcopy < $argv[1]
+        if test -e $argv[1]
+          pbcopy < $argv[1]
+        else
+          echo $argv[1] | pbcopy
+        end
     end
   else
     echo "Well this is embarrassing... I can only copy one file at a time."

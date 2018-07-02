269c5b57b34865dcd1e413a379f2a13dc0e58159
Dennis Ideler <ideler.dennis@gmail.com>
2018-07-02 23:27:24 +0100

    :fish: Add alias for youtube-dl

---
+++ .config/fish/functions/y.fish
@@ -0,0 +1,9 @@
+function y -d "Smart alias for youtube-dl"
+    for path in $argv
+        __y_download $path
+    end
+end
+
+function __y_download --argument path
+    youtube-dl --ignore-config --no-mtime --output "~/Music/%(title)s.%(ext)s" --extract-audio --audio-format mp3 $path
+end

0bbf3da58f8e4b257c2ae9829951d89e6a969216
Dennis Ideler <ideler.dennis@gmail.com>
2018-07-02 23:31:00 +0100

    :fish: Quiet execution for youtube-dl alias

--- .config/fish/functions/y.fish
+++ .config/fish/functions/y.fish
@@ -5,5 +5,5 @@ function y -d "Smart alias for youtube-dl"
 end
 
 function __y_download --argument path
-    youtube-dl --ignore-config --no-mtime --output "~/Music/%(title)s.%(ext)s" --extract-audio --audio-format mp3 $path
+    youtube-dl --ignore-config --quiet --no-mtime --output "~/Music/%(title)s.%(ext)s" --extract-audio --audio-format mp3 $path
 end

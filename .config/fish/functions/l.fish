596091f2b1a6dc50d3e6af97801a06f3038c2e08
Dennis Ideler <ideler.dennis@gmail.com>
2018-10-14 18:15:16 +0100

    :fish: l can be used against other directories
    And validate that given count is an integer of minimum value 1.

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,12 +1,19 @@
 function __l_print_help
     printf "Usage: l [FILE_COUNT]\n\n"
+    printf "Description:\n"
+    printf "    Prints recently modified files\n\n"
     printf "Examples:\n"
-    printf "  l   # Last 10 modified files in the current directory\n"
-    printf "  l 5 # Last 5 modified files in the current directory\n"
+    printf "  l                    # Last few files in current directory\n"
+    printf "  l -n 5               # Last 5 files in current directory\n"
+    printf "  l ~/Music            # Last few files in another directory\n"
+    printf "  l ~/Music --count 5  # Last 5 files in another directory\n\n"
+    printf "Options:\n"
+    printf "   -h, --help   Prints help and exits\n"
+    printf "   -n, --count  Limits file count (minimum 1)"
 end
 
 function l --description "Show last n files by newest to oldest"
-  set --local options 'h/help'
+  set --local options 'h/help' 'n/count=!_validate_int --min 1'
   argparse $options -- $argv
 
   if set --query _flag_help
@@ -14,9 +21,11 @@ function l --description "Show last n files by newest to oldest"
     return 0
   end
 
-  if test (count $argv) -eq 1
-    ls -t | head -n $argv[1]
+  set --query _flag_count; or set --local _flag_count 10
+
+  if test -d $argv[1]
+    ls -t $argv[1] | head -n $_flag_count
   else
-    ls -t | head
+    ls -t | head -n $_flag_count
   end
 end

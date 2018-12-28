e3e7377bc7063c273462538250a6c74ff94ffcb5
Dennis Ideler <ideler.dennis@gmail.com>
2018-12-28 14:05:41 +0000

    Remove export when setting fish_user_paths
    According to fish docs, it's meant to be a universal variable only.
    
    https://fishshell.com/docs/current/tutorial.html#tut_path

--- .config/fish/functions/add_to_user_path.fish
+++ .config/fish/functions/add_to_user_path.fish
@@ -1,7 +1,7 @@
 function add_to_user_path --description "Persistently prepends to your PATH via fish_user_paths"
   for path in $argv
     if not contains $path $fish_user_paths
-      set --universal --export fish_user_paths $path $fish_user_paths
+      set --universal fish_user_paths $path $fish_user_paths
     end
   end
 end

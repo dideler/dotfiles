392987a11d80df035911ceaaca84c34bf55624a0
Dennis Ideler <ideler.dennis@gmail.com>
2019-02-23 20:41:56 +0000

    :fish: Add help for index function

--- .config/fish/functions/index.fish
+++ .config/fish/functions/index.fish
@@ -1,4 +1,34 @@
 function index --description "Shows character indexes of a string"
+  argparse 'h/help' -- $argv
+
+  if set --query _flag_help
+    echo "Usage: index [OPTION] [STRINGS]"
+    echo
+    echo "Description:"
+    echo "    Prints the index of characters in a string."
+    echo "    Sensitive strings can be given as a prompt,"
+    echo "    to avoid appearing in the shell's history."
+    echo
+    echo "Examples:"
+    echo
+    echo "    \$ index ab cd"
+    echo "    1: a"
+    echo "    2: b"
+    echo
+    echo "    1: c"
+    echo "    2: d"
+    echo
+    echo "    \$ index"
+    echo "    ▶ ●●●"
+    echo "    1: x"
+    echo "    2: y"
+    echo "    3: z"
+    echo
+    echo "Options:"
+    echo "    -h, --help      Prints help and exits"
+    return 0
+  end
+
   set --local argc (count $argv)
 
   if test $argc -eq 0

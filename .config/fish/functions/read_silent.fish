2ac9aac928a1459e06ca95efad0f46f896a08325
Dennis Ideler <ideler.dennis@gmail.com>
2019-09-07 23:58:51 +0100

    Rewrite read_silent help and show help on error

--- .config/fish/functions/read_silent.fish
+++ .config/fish/functions/read_silent.fish
@@ -5,18 +5,21 @@ function read_silent -d "Read sensitive hidden input into a variable"
   set --local parse_status $status
   test $parse_status -ne 0 && return $parse_status
 
-  if set --query _flag_help
+  function _read_silent_help
     echo "Usage: read_silent [OPTION] VARIABLE"
     echo
     echo "Description:"
-    echo "    Read sensitive information without displaying the characters"
-    echo "    on screen. Fish's `read --silent` masks the characters instead of"
-    echo "    hiding them. Whereas read_silent hides the characters, similar to"
-    echo "    Bash's `read -s`."
+    echo "    Reads sensitive information into a variable."
+    echo "    The input stays hidden, similar to bash's `read -s` behaviour."
+    echo "    Unlike fish's `read -s` which shows masked input characters."
     echo
     echo "Options:"
     echo "    -h, --help            Prints help and exits"
     echo "    -p STR, --prompt=STR  Uses the string as the prompt"
+  end
+
+  if set --query _flag_help
+    _read_silent_help
   end
 
   set --local argc (count $argv)
@@ -28,7 +31,7 @@ function read_silent -d "Read sensitive hidden input into a variable"
     head -n 1 | read -g $argv[1]
     stty echo
   else
-    echo "Error: read_silent requires a single variable to store the input"
+    _read_silent_help
     return 1
   end
 end

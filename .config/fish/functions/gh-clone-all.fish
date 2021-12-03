95874bd52ca3072690fe2f6781e81597a014cb46
Dennis Ideler <ideler.dennis@gmail.com>
2021-12-03 01:32:33 +0000

    Add clone script
    Useful when switching to a new computer.

---
+++ .config/fish/functions/gh-clone-all.fish
@@ -0,0 +1,16 @@
+function gh-clone-all --description "Clone all GH repos of the user and their orgs"
+  gh auth status >/dev/null || {
+    echo "Must authenticate via GitHub CLI: gh auth login" 1>&2
+    exit 1
+  }
+
+  set -l orgs (gh api user/orgs -q '.[].login')
+  set -l owners dideler $orgs
+
+  for owner in $owners
+    set -l namespace_dir ~/github.com/$owner
+    mkdir -p $namespace_dir && pushd $namespace_dir
+    gh repo list $owner --limit 1000 | awk '{print $1; }' | xargs -L1 gh repo clone
+    popd
+  end
+end

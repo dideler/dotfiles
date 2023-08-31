9eccb346764e146ff8435eaacf956f9486c61072
Dennis Ideler <ideler.dennis@gmail.com>
2023-08-31 16:44:16 +0100

    comprss delegates to specific functions

--- .config/fish/functions/comprss.fish
+++ .config/fish/functions/comprss.fish
@@ -1,6 +1,6 @@
 # Collection of compression helper functions for a variety of file types.
 
-function pngcompress -d "Reduce size of PNG files given or all PNG files in current directory"
+function pngcompress -d "Reduce PNG file size"
   function __help
     echo "Usage: pngcompress [file ...] [options]"
     echo "Options:"
@@ -42,5 +42,46 @@ function pngcompress -d "Reduce size of PNG files given or all PNG files in curr
 end
 
 function comprss --description "Compress a variety of file types" # The name 'compress' is taken by a BSD tool.
-  # TODO: create specific compression functions to delegate to based on filetype
+  function __help
+    echo "Usage: comprss [file ...] [options]"
+    echo "Options:"
+    echo "  -h, --help    Show this help message and exit"
+    echo "  -o, --output  Output to given path (instead of overwriting existing file). Limits input to single file"
+  end
+
+  set --local args $argv # Because argparse mutates $argv
+  set --local options 'h/help' 'o/output='
+  argparse --max-args=2 $options -- $argv
+  or return
+
+  if set --query _flag_help
+    __help
+    return 0
+  end
+
+  if set --query _flag_output
+    if test (count $argv) -gt 1
+      echo "Error: Only one input file is allowed when using the -o/--output option" >&2
+      return 1
+    end
+
+    switch (string lower (extension $argv[1]))
+      case 'png'
+        pngcompress $args
+      case '*'
+        echo "Error: Filetype unsupported" >&2
+        return 1
+    end
+  else
+    for file in $argv
+      switch (string lower (extension $file))
+        case 'png'
+          pngcompress $file
+        case '*'
+          echo "Error: Filetype unsupported for $file" >&2
+          return 1
+      end
+      echo "Compressed $file"
+    end
+  end
 end

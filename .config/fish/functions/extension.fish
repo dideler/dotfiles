00e1ea3aee4be84729b5f5ca511186ff8fb3a9e2
Dennis Ideler <ideler.dennis@gmail.com>
2023-09-07 00:50:17 +0100

    Add upper and lower options to extension func

--- .config/fish/functions/extension.fish
+++ .config/fish/functions/extension.fish
@@ -1,5 +1,4 @@
-function extension -d "Returns the extension given a file"
-
+function extension -d "Returns the extension given a filepath"
   function __help
     echo "Returns the probable extension from a given filepath, e.g. 'file.txt' => 'txt'
 
@@ -8,9 +7,11 @@ Usage: "(status current-command)" [options] [file]
 Options:
   -h, --help    Show this help message and exit
   -m, --multi   Extract multiple extensions, e.g. 'file.tar.gz' => 'tar.gz'
+  -l, --lower   Returns the extension in lowercase
+  -u, --upper   Returns the extension in uppercase"
   end
 
-  set --local options 'h/help' 'm/multi'
+  set --local options 'h/help' 'm/multi' 'l/lower' 'u/upper'
   argparse --max-args=1 $options -- $argv
   or return
 
@@ -22,17 +23,27 @@ Options:
     return 0
   end
 
+  set --local ext
+
   if set --query _flag_multi
-    string split '.' $argv[1] --no-empty --max 1 --fields 2
-    and return 0
-
-    printf "Error: '%s' is not valid\n" $argv[1] >&2
-    return 1
+    set ext (string split '.' $argv[1] --no-empty --max 1 --fields 2)
+    or begin
+      printf "Error: '%s' is not valid\n" $argv[1] >&2
+      return 1
+    end
   else
-    string match --regex --groups-only '^\.?[\w,\s-]+(\.[A-Za-z]+)+$' $argv[1] | string split '.' --fields 2
-    and return 0
+    set ext (string match --regex --groups-only '^\.?[\w,\s-]+(\.[A-Za-z]+)+$' $argv[1] | string split '.' --fields 2)
+    or begin
+      printf "Error: '%s' is not valid\n" $argv[1] >&2
+      return 1
+    end
+  end
 
-    printf "Error: '%s' is not valid\n" $argv[1] >&2
-    return 1
+  if set --query _flag_lower
+    string lower $ext
+  else if set --query _flag_upper
+    string upper $ext
+  else
+    echo $ext
   end
 end

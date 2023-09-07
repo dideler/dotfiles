30a0a85d6d8a64098973163fc1a2a231c3c5cf5b
Dennis Ideler <ideler.dennis@gmail.com>
2023-09-07 15:05:27 +0100

    Add jpgcompress function
    Note the jpg/jpeg extension matching can also be done in a single line
    `string match -rq '^.+\.(jpg|jpeg)$' (string lower $argv[1])`
    but it's more complex has multiple matches so I avoided it.
    
    Compared several tools for compressing JPEGs. One called jpregrescan
    (also available on homebrew) had similar results.

--- .config/fish/functions/comprss.fish
+++ .config/fish/functions/comprss.fish
@@ -46,6 +46,51 @@ function pdfcompress -d "Reduce PDF file size"
   end
 end
 
+function jpgcompress -d "Reduce JPG file size"
+  function __help
+    echo "Usage: "(status current-command)" [file ...] [options]"
+    echo "Options:"
+    echo "  -h, --help    Show this help message and exit"
+    echo "  -o, --output  Output to given path (instead of overwriting existing file). Limits input to single file"
+  end
+
+  set --local options 'h/help' 'o/output='
+  argparse --max-args=2 $options -- $argv
+  or return
+
+  set --local argc (count $argv)
+
+  if set --query _flag_help
+  or test $argc -eq 0
+    __help
+    return 0
+  end
+
+  function __validate! -d "Validate file existence, extension, and type"
+    test -f $argv[1] && \
+    string match -q '*.jpg' (string lower $argv[1]) || \
+    string match -q '*.jpeg' (string lower $argv[1]) && \
+    string match -rq "^$argv[1]: JPEG image data" (file $argv[1])
+    or echo "Error: '$argv[1]' is not a valid JPEG file" >&2
+    and return 1
+  end
+
+  if set --query _flag_output
+    if test $argc -gt 1
+      echo "Error: Only one input file is allowed when using the -o/--output option" >&2
+      return 1
+    end
+
+    __validate! $argv[1] or return
+    jpegtran -copy none -optimize -progressive -outfile $_flag_output $argv[1]
+  else
+    for file in $argv
+      __validate! $file or continue
+      jpegtran -copy none -optimize -progressive -outfile $argv[1] $argv[1]
+    end
+  end
+end
+
 function pngcompress -d "Reduce PNG file size"
   function __help
     echo "Usage: "(status current-command)" [file ...] [options]"
@@ -118,6 +163,8 @@ function comprss --description "Compress a variety of file types" # The name 'co
     end
 
     switch (extension --lower $argv[1])
+      case 'jpg' 'jpeg'
+        jpgcompress $args
       case 'pdf'
         pdfcompress $args
       case 'png'
@@ -129,6 +176,8 @@ function comprss --description "Compress a variety of file types" # The name 'co
   else
     for file in $argv
       switch (extension --lower $file)
+        case 'jpg' 'jpeg'
+          jpgcompress $file
         case 'pdf'
           pdfcompress $file
         case 'png'

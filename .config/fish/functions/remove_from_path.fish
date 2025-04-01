bece7b4fbaf09c6bbf49ea1e96835ecfc945b62f
Dennis Ideler <dennis.ideler@moderntreasury.com>
2025-04-01 16:46:43 +0100

    Remove deprecated path functions
    We can use fish_add_path and fish_remove_path from now on.
    
    Note that fish_remove_path doesn't give you the granularity to choose
    if you want to remove from $PATH or $fish_user_paths but I don't think
    there will ever be a scenario where we want to remove from one and not
    the other. And if there is, can add an option to that function.

--- .config/fish/functions/remove_from_path.fish
+++
@@ -1,7 +0,0 @@
-function remove_from_path --description "Removes the given paths from PATH"
-  for path in $argv
-    if set --local index (contains --index $path $PATH)
-      set --erase PATH[$index]
-    end
-  end
-end
--- .config/fish/functions/remove_from_user_path.fish
+++
@@ -1,7 +0,0 @@
-function remove_from_user_path --description "Removes the given paths from fish_user_paths"
-  for path in $argv
-    if set --local index (contains --index $path $fish_user_paths)
-      set --erase --universal fish_user_paths[$index]
-    end
-  end
-end
